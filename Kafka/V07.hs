f6e043665093664288a27654d537d3b96ff7ace6
Abhinav Gupta <mail@abhinavg.net>
2014-11-26 21:54:07 -0800

    Add Kafka.V07.Internal.
    It contains most of the core types. These types will most likely not change
    regardless of what the client interface looks like.

--- Kafka/V07.hs
+++ Kafka/V07.hs
@@ -1,20 +1,17 @@
-{-# LANGUAGE GeneralizedNewtypeDeriving #-}
-{-# LANGUAGE NamedFieldPuns             #-}
+{-# LANGUAGE NamedFieldPuns #-}
 module Kafka.V07 where
 
 import Control.Applicative
 import Data.ByteString           (ByteString)
-import Data.Digest.CRC32         (crc32)
-import Data.Time                 (UTCTime)
-import Data.Time.Clock.POSIX     (utcTimeToPOSIXSeconds)
-import Data.Word                 (Word32, Word64)
+import Data.Word                 (Word32)
 import Network.Socket.ByteString (recv)
 
 import qualified Control.Exception     as E
-import qualified Data.ByteString       as B
 import qualified Data.ByteString.Char8 as B8
 import qualified Data.Serialize        as C
 import qualified Network.Socket        as N
+
+import Kafka.V07.Internal
 
 -- TODO break into modules
 -- TODO This probably belongs in Kafka.V07.Internal or something.
@@ -34,8 +31,6 @@ data Connection = Connection {
 connect :: ByteString -> Int -> IO Connection
 connect host port = do
     -- TODO Accept a config instead. Config will specify the buffer size.
-    -- Connection can contain the send and recv functions based on buffer
-    -- size.
     (addrInfo:_) <- N.getAddrInfo
                         (Just hints)
                         (Just $ B8.unpack host)
@@ -69,122 +64,14 @@ recvResponse Connection{connSocket} = do
     -- TODO Maybe use something like a ProtocolError for parse errors.
     (respLen, err) <- either fail return $
         C.runGet ((,) <$> getLength
-                      <*> getError) header
+                      <*> C.get) header
     body <- recv connSocket (respLen - 2)
     return $ maybe (Right body) Left err
   where
     getLength = fromIntegral <$> C.getWord32be
 
-data Error
-    = UnknownError
-    | OffsetOutOfRangeError
-    | InvalidMessageError
-    | WrongPartitionError
-    | InvalidFetchSizeError
-  deriving (Show, Read, Eq)
-
--- | Reader for 'Error's.
---
--- Produces 'Nothing' if there was no error.
-getError :: C.Get (Maybe Error)
-getError = do
-    errorCode <- C.getWord16be
-    case errorCode of
-        0  -> return Nothing
-        1  -> return $ Just OffsetOutOfRangeError
-        2  -> return $ Just InvalidMessageError
-        3  -> return $ Just WrongPartitionError
-        4  -> return $ Just InvalidFetchSizeError
-        -1 -> return $ Just UnknownError
-        _  ->   fail $ "Unknown error code: " ++ show errorCode
-
-newtype Topic = Topic ByteString
-    deriving (Show, Read, Eq)
-
-instance C.Serialize Topic where
-    put (Topic topic) = do
-        C.putWord16be (fromIntegral $ B.length topic)
-        C.putByteString topic
-    get = do
-        topicLength <- fromIntegral <$> C.getWord16be
-        Topic <$> C.getByteString topicLength
-
-newtype Offset = Offset Word64
-    deriving (Show, Read, Eq, C.Serialize)
-
-newtype Partition = Partition Word32
-    deriving (Show, Read, Eq, C.Serialize)
-
 data Request a = Request Topic Partition a
 type Response a = Either Error a
-
-data Compression
-    = NoCompression
-    | GzipCompression
-    | SnappyCompression
-  deriving (Show, Read, Eq)
-
-instance C.Serialize Compression where
-    put NoCompression = C.putWord8 0
-    put GzipCompression = C.putWord8 1
-    put SnappyCompression = C.putWord8 2
-
-    get = C.getWord8 >>= \i -> case i of
-        0 -> return NoCompression
-        1 -> return GzipCompression
-        2 -> return SnappyCompression
-        _ -> fail $ "Invalid compression code: " ++ show i
-
--- TODO change to not use record fields. Hide Message constructor from user.
-data Message = Message {
-    messageCompression :: {-# UNPACK #-} !Compression
-  , messagePayload     :: {-# UNPACK #-} !ByteString
-  } deriving (Show, Read, Eq)
-
-instance C.Serialize Message where
-    put message = do
-        C.putWord32be (fromIntegral messageLength)
-        C.putWord8 1 -- Magic
-        C.put compression
-        C.putWord32be checksum
-        C.putByteString payload
-      where
-        Message {
-            messageCompression = compression
-          , messagePayload = payload
-          } = message
-        checksum = crc32 payload
-        messageLength = B.length payload
-            + 4 -- checksum
-            + 1 -- compression
-            + 1 -- magic
-
-    get = C.getWord32be >>= \messageLength -> do
-        magic <- C.getWord8
-        compression <- case magic of
-            0 -> return NoCompression
-            1 -> C.get
-            _ -> fail $ "Unknown magic code: " ++ show magic
-        _ <- C.getWord32be -- checksum ignored
-        let remainingLength = messageLength -
-                ( 4 -- checksum
-                + 1 -- magic
-                + (if magic == 0 then 0 else 1) -- compression
-                )
-        payload <- C.getByteString (fromIntegral remainingLength)
-        return $ Message compression payload
-
-data OffsetsTime
-    = OffsetsLatest
-    | OffsetsEarliest
-    | OffsetsBefore UTCTime
-  deriving (Show, Read, Eq)
-
-putOffsetsTime :: C.Putter OffsetsTime
-putOffsetsTime OffsetsLatest = C.putWord64be (-1)
-putOffsetsTime OffsetsEarliest = C.putWord64be (-2)
-putOffsetsTime (OffsetsBefore t) =
-    C.putWord64be . round . (* 1000) $ utcTimeToPOSIXSeconds t
 
 newtype Produce = Produce {
     produceMessages :: [Message]
